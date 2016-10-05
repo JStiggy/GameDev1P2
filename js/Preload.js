86361f019f19c2e63400b59868691d111c86b65b
John Steigerwald <johnstiggy@gmail.com>
2016-10-05 16:47:47 -0400

    Implemented Children Section art assets
    Added all files for the Children section

--- js/Preload.js
+++ js/Preload.js
@@ -13,7 +13,7 @@ BookWyrm.Preload.prototype = {
     //Load all Level JSON files
     this.load.tilemap('level1', 'assets/tilemaps/TestMap.json', null, Phaser.Tilemap.TILED_JSON);
     this.load.json('level1Data', 'assets/tilemaps/TestMap.json');
-    
+
     //Load all art assets
     this.load.image('gameTiles', 'assets/tiles/Tileset.png');
     this.load.image('cart0', 'assets/tiles/CartV.png');
@@ -23,6 +23,18 @@ BookWyrm.Preload.prototype = {
     this.load.image('book', 'assets/tiles/Book.png');
     this.load.image('exit', 'assets/tiles/Exit.png');
     this.load.image('librarian', 'assets/tiles/Librarian.png');
+
+    this.load.image('cBL', 'assets/tiles/Children Section/BottomLeft.png');
+    this.load.image('cBR', 'assets/tiles/Children Section/BottomRight.png');
+    this.load.image('cBVE', 'assets/tiles/Children Section/BottomVerticalEnd.png');
+    this.load.image('cF', 'assets/tiles/Children Section/Floor.png');
+    this.load.image('cLVE', 'assets/tiles/Children Section/LeftVerticalEnd.png');
+    this.load.image('cMC', 'assets/tiles/Children Section/MiddleConnect.png');
+    this.load.image('cRVE', 'assets/tiles/Children Section/RightVerticalEnd.png');
+    this.load.image('cTL', 'assets/tiles/Children Section/TopLeft.png');
+    this.load.image('cTR', 'assets/tiles/Children Section/TopRight.png');
+    this.load.image('cTVE', 'assets/tiles/Children Section/TopVerticalEnd.png');
+    this.load.image('cVM', 'assets/tiles/Children Section/VerticalMiddle.png');
   },
   create: function() {
     //Start the actual Game State
