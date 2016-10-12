5dd55abf5abf483983a571c08c0016defb9e4a77
Rikusaber <huange2@rpi.edu>
2016-10-11 22:10:55 -0400

    logo animation

--- js/Title.js
+++ js/Title.js
@@ -11,7 +11,13 @@ BookWyrm.Title.prototype = {
 function create()
 {
     //logo
-    BookWyrm.game.add.sprite(BookWyrm.game.camera.width/2-190, 250, 'logo');
+    logos = BookWyrm.game.add.sprite(BookWyrm.game.camera.width/2, 275, 'logo');
+    logos.anchor.set(.5);
+    logos.scale.setTo(1.5,1.5);
+    turnR = BookWyrm.game.add.tween(logos).to( { angle: 20 }, 5000, Phaser.Easing.Exponential.In, true);
+    turnL = BookWyrm.game.add.tween(logos).to( { angle: -20 }, 5000, Phaser.Easing.Exponential.In, false);
+    turnR.onComplete.add(function(){turnL.start()}, logos);
+    turnL.onComplete.add(function(){turnR.start()}, logos);
 
 
 
